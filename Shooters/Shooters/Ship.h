7f79a1f71c5ec8dd40446169b479ee823239a478
moses <moseslinda@outlook.com>
2015-04-26 00:47:55 +0200

    moving ship

--- Shooters/Shooters/Ship.h
+++ Shooters/Shooters/Ship.h
@@ -1,11 +1,48 @@
-#pragma once
+enum IDS{ PLAYER, BULLET, ENAMY };
 class Ship
 {
+private:
+	const int width = 800;
+	const int height = 450;
+	int ID;
+	int x;
+	int y;
+	int lives;
+	int speed;
+	int score;
+	int boundx;
+	int boundy;
+
 public:
-	Ship(int=400,int=240,int=0);
+	Ship(int = 400, int = 240, int = 0);
 	~Ship();
-	void Drawspaceship();
-	void 
+	void DrawSpaceShip();
+	void MoveshipUp();
+	void MoveshipDown();
+	void MoveshipRight();
+	void MoveshipLeft();
+
+};
+class Bullet
+{
+private:
+	const int width = 800;
+	const int height = 450;
+	int ID;
+	int x;
+	int y;
+	bool live;
+	int speed;
+public:
+	Bullet();
+	~Bullet();
+	void initBullet(Bullet bullet[], int size);
+	void DrawBullet(Bullet bullet[], int size);
+	void FireBullet(Bullet bullet[], int size, Ship &ship);
+	void UpdateBullet(Bullet bullet[], int size);
+
+
+
 
 };
 

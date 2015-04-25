7ca54af8c76bbd495aa5c64b34247179d0e0ccf1
moses <moseslinda@outlook.com>
2015-04-25 23:57:49 +0200

    first commit

---
+++ Shooters/Shooters/Main.cpp
@@ -0,0 +1,13 @@
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_native_dialog.h>
+#include<allegro5/allegro_primitives.h>
+#include"Ship.h"
+
+int main(int argc, char **argv)
+{
+
+
+
+
+	return 0;
+}
